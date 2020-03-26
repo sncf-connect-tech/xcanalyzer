7b533249d87afea8e73d2a5ee8989ea598f484b1
Deffrasnes Ghislain <gdeffrasnes@oui.sncf>
2020-03-26 18:59:34 +0100

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xcanalyzer-gdeff", # Replace with your own username
+    version="0.0.1",
+    author="Ghislain Deffrasnes",
+    author_email="gdeffrasnes@oui.sncf",
+    description="A project that aims to make static analysis on a Xcode project.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/voyages-sncf-technologies/xcanalyzer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: BSD 3-Clause License :: E-Voyageurs Technologies",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
